f72198094997cdc66b57e9b91e4f432f84704469
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2017-12-17 14:22:02 -0500

    Created a sample scene
    Integrated my destruction script with the other scripts in scene
    created.
    
    full testing not done as I'm working from an airport on a laptop, so no
    mouse to test all directions.

---
+++ Assets/Scripts/PlayerForTeamworks.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerForTeamworks : MonoBehaviour {
+
+
+    public int numberOfSnowflakes = 0;
+
+    private Vector3 userPosition;
+    private Vector3 pastPosition;
+    public float speed = 0.5f;
+    private List<GameObject> allFlakes = new List<GameObject>();
+
+    private void Start()
+    {
+		numberOfSnowflakes = getCurrentSnowflakeCountOnField();
+    }
+
+    private void Update()
+    {
+
+       // Camera.main.transform.parent.transform.position += (Camera.main.transform.forward * Time.deltaTime *speed);
+        //Translate(new Vector3(0, 0, Time.deltaTime * speed), Space.World);
+
+        destroyflakesPastUser();
+		numberOfSnowflakes = getCurrentSnowflakeCountOnField();
+    }
+
+
+    private void destroyflakesPastUser() {
+		List<GameObject> objectsToDestroy = new List<GameObject>();
+        // Any flake at a point on Z axis behind the Camera would be destroyed
+		foreach (GameObject a in allFlakes.ToArray()) {
+			
+            if (a!=null &&(a.transform.position.y < Camera.main.transform.parent.transform.position.y)) {                
+            	objectsToDestroy.Add(a);				
+				Debug.Log("Marked for destruction: "+a);
+				allFlakes.Remove(a);
+
+            }
+        }
+		foreach (GameObject a in objectsToDestroy.ToArray()) {            					
+			Debug.Log("Destroying  "+a);
+				Destroy(a);
+        }
+    }
+ 
+    public void incrementflakeCount() {
+		numberOfSnowflakes++;
+    }
+
+    public int getflakeCount() {
+		return numberOfSnowflakes;
+    }
+
+    public int getCurrentSnowflakeCountOnField()
+    {        
+		allFlakes = GameObject.FindGameObjectsWithTag("Snowflake").ToList<GameObject>(); ;
+		return allFlakes.Count; 
+    }
+ }
--- Assets/Scripts/SpawnSnowFlakes.cs
+++ Assets/Scripts/SpawnSnowFlakes.cs
@@ -6,7 +6,6 @@ public class SpawnSnowFlakes : MonoBehaviour {
 
 	 public GameObject snowFlake;  // SnowFlake Object in Scene
 	 public float spawnTime;       // How long between each spawn.
-	 public float fallSpeed;       // The speed of falling Apples
 	 private float timer;          // counting timer, reset after calling SpawnRandom() function
 	 private int randomNumber;     // variable for storage of an random Number
 
@@ -22,12 +21,12 @@ public class SpawnSnowFlakes : MonoBehaviour {
 
 	 public void SpawnRandom()
 	 {
-		  float screenX = Random.Range(0,Screen.width);
-		  float screenY = Screen.height;
+		float screenX = Random.Range(Camera.main.transform.parent.transform.position.x-5,Camera.main.transform.parent.transform.position.x+5);//Screen.width);
+		  float screenY = 100;//Screen.height;
 		  float ScreenZ = Random.Range(Camera.main.farClipPlane/2, Camera.main.farClipPlane);
 
 		  //Creating random Vector3 position
-		  Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(screenX, screenY, ScreenZ));
+		Vector3 screenPosition = new Vector3(screenX, screenY, ScreenZ);//Camera.main.ScreenToWorldPoint(new Vector3(screenX, screenY, ScreenZ));
 
 		  //Instantiation of the Snowflake Object
 		  Instantiate(snowFlake,screenPosition,Quaternion.Euler(new Vector3(0, 90, 90)));
